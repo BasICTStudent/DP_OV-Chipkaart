23db751e989cd358a3dd0ccb0237a5560879613a
Bas Valkonet <bas.valkonet@student.hu.nl>
2021-09-09 11:17:23 +0200

    aanpassing main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -48,8 +48,7 @@ public class Main {
 
         // Maak een nieuwe reiziger aan en persisteer deze in de database
         String gbdatum = "1981-03-14";
-//        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
-        Reiziger sietske = new Reiziger(88, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
         System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
         rdao.save(sietske);
         reizigers = rdao.findAll();
